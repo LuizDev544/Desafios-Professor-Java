888f1768fd887a784e9d08f7171747f9c5bcdbe5
Luizfernando791 <luizfernando.carrupt@gmail.com>
2025-04-02 14:44:09 -0300

    Create Desafio1.java

---
+++ Exercicio2Parte1/Desafio1.java
@@ -0,0 +1,14 @@
+package Exercicio2Parte1;
+import java.util.Scanner;
+
+public class Desafio1 {
+    public static void main(String[] args) {
+        System.out.println("||Sistemas de notas DETALHADo||");
+
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}
