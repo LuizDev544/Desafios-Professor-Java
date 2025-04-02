d294154aa4d73f5bddee61e410562534b67deb05
Luizfernando791 <luizfernando.carrupt@gmail.com>
2025-04-02 13:52:06 -0300

    ee

---
+++ Exercicio1/Desafio1.java
@@ -0,0 +1,23 @@
+package Exercicio1;
+import java.util.Scanner;
+
+public class Desafio1 {
+    public static void main(String[] args) {
+        System.err.println("||Conversão de temperatura||");
+        System.err.println("--Conversão de C para F e K --");
+
+        Scanner sc = new Scanner(System.in);
+
+        double Celcius;
+        System.err.println("Informe o numero de graus Celcius para conversão:");
+        Celcius = sc.nextDouble();
+
+        double F = (Celcius * 1.8) + 32;
+        double K = Celcius + 273.15;
+
+        System.out.println("A conversão para Kelvin foi de: " + K +);
+        System.out.println("A conversão para Farenheit foi de: " + F +);
+
+        sc.close();
+    }
+}
