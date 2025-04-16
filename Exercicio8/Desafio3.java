1d06798092da99e2201f0758d73ace3c2a063786
Luizfernando791 <luizfernando.carrupt@gmail.com>
2025-04-16 14:49:12 -0300

    Create Desafio3.java

---
+++ Exercicio8/Desafio3.java
@@ -0,0 +1,17 @@
+package Exercicio8;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Desafio3 {
+    public static void main(String[] args) {
+        System.out.println("||Palavras de comprimento Par ou Impar||");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um numero de palavras utilizando a virgula:");
+
+        sc.close();
+        }
+}
