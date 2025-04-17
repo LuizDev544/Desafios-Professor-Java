5f20a0e02167d67ecfd44aa5accf25e4dc601691
Luizfernando791 <luizfernando.carrupt@gmail.com>
2025-04-17 14:26:08 -0300

    aprimorando

--- POO/FormaEstruturada.java
+++ POO/FormaEstruturada.java
@@ -1,31 +1,24 @@
-package desafios;
+package POO;
 
-public class desafio1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// forma estruturada
+public class FormaEstruturada {
+    public static void main(String[] args) {
 		double JanAlimentacao , JanAgua , JanLuz , FevAlimentacao , FevAgua , FevLuz;
-		
+
 		// gastos janeiro
-		
 		JanAlimentacao = 1000;
 		JanAgua = 350 ;
 		JanLuz = 500;
 		
 		//Gastos Fevereiro
-		
 		FevAlimentacao = 3500;
 		FevLuz = 100;
 		FevAgua = 400;
 		
 		// Gasto total
-		
 		double gastoTotalJan = JanAlimentacao + JanLuz + JanAgua;
 		double gastoTotalFev = FevAlimentacao + FevLuz + FevAgua;
 		
 		// Print 
-		
 		if (gastoTotalJan > gastoTotalFev) {
 			System.out.println("O gasto de janeiro foi maior");
 		}else if(gastoTotalFev > gastoTotalJan){
@@ -34,5 +27,4 @@ public class desafio1 {
 			System.out.println("Error");
 		}
 	}
-
 }
